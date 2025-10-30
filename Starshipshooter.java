443de314e3d6e45e45e6fa034338a0390835443a
NineZaFire <s6704062616079@email.kmutnb.ac.th>
2025-10-30 18:17:29 +0700

    Add files via upload

---
+++ Starshipshooter.java
@@ -0,0 +1,39 @@
+package starshipshooter;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Starshipshooter extends JFrame implements ActionListener {
+    MainMenu homeMenu = new MainMenu();;
+    static gameRun stageloop = new gameRun();;
+    static Starshipshooter gamerun;
+    // ========= Main ============
+    public static void main(String[] args) {
+         gamerun = new Starshipshooter();
+    }
+
+    public Starshipshooter() {
+        setTitle("Starship Shooter");
+        setSize(1100, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        add(homeMenu);
+        homeMenu.start.addActionListener(this);
+        homeMenu.quit.addActionListener(this);
+
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == homeMenu.start) {
+            remove(homeMenu);
+            add(stageloop);
+            revalidate();
+            repaint();
+        } else if (e.getSource() == homeMenu.quit) {
+            System.exit(0);
+        }
+    }
+}
